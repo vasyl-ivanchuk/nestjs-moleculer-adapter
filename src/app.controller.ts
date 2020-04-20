94c96d1a33b01d51cbc37619e0f1356818db1f86
vasyl-ivanchuk <vasyl.ivanchuk@gmail.com>
2020-04-20 14:17:42 +0300

    feat: use moleculer adapter from nest controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MoleculerAdapter } from './moleculer/adapter';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly moleculerAdapter: MoleculerAdapter
+  ) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('moleculer')
+  getMoleculerData(): any {
+    return this.moleculerAdapter.call('test.service.getMessage');
+  }
+
+  @Get('emit')
+  emitMoleculerEvent(): Promise<void> {
+    return this.moleculerAdapter.emit('entity.created', { entityId: 'id-1', timestamp: new Date() });
   }
 }
